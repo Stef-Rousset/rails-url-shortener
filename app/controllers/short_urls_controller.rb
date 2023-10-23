0ae1b841eefb8f8c563e396d6545d98b36d90015
stephanie rousset <strabon@club-internet.fr>
2023-10-23 10:46:05 +0200

    add rescue in short_urls controller

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,5 +1,6 @@
 class ShortUrlsController < ApplicationController
   before_action :authenticate_user!
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def index
     @urls = ShortUrl.all.where(user_id: current_user.id)
@@ -24,7 +25,7 @@ class ShortUrlsController < ApplicationController
   end
 
   def url_shortened
-    @url = ShortUrl.find_by_tiny_url(params[:url_shortened])
+    @url = ShortUrl.find_by_tiny_url!(params[:url_shortened]) # bang added to raise an ActiveRecord::RecordNotFound error
     redirect_to @url.long_url, allow_other_host: true
   end
 
@@ -32,5 +33,9 @@ class ShortUrlsController < ApplicationController
 
   def short_url_params
     params.require(:short_url).permit(:long_url, :user_id)
+  end
+
+  def not_found(exception)
+    redirect_to short_urls_path, alert: t('record_not_found', my_object: ShortUrl.model_name.name, params: (params[:id] || params[:url_shortened]).to_s)
   end
 end
