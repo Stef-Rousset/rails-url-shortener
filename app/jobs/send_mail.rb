d58ddf5724304cc02f075fc9da65353363f85f55
stephanie rousset <strabon@club-internet.fr>
2023-10-26 17:14:22 +0200

    update send_mail method

--- app/jobs/send_mail.rb
+++ app/jobs/send_mail.rb
@@ -1,9 +1,10 @@
 require 'sidekiq-scheduler'
 
+# Job to send news mail to user
 class SendMail < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform
     User.joins(:sources).distinct.each do |user|
       UserMailer.with(user: user).news_email.deliver_now
     end
