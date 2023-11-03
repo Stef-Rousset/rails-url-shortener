74a1c083ca90e8011b40b700833436de0c3fc549
stephanie rousset <strabon@club-internet.fr>
2023-11-03 16:13:21 +0100

    add .env file, and smtp setup for sending email with gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails_url_shortener_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://shared-tools-6d7408ce9f86.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'shared-tools-6d7408ce9f86.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_ADDRESS'],
+    password:             ENV['GMAIL_APP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
