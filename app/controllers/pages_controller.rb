bd3abbc00b84190dc15a805ade1633800555e87f
stephanie rousset <strabon@club-internet.fr>
2023-11-09 11:15:41 +0100

    add authentication to weather action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 # controller for Pages
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: %i[weather]
+
   def home
     @user = current_user if user_signed_in?
   end
