d58087f4f65455cc148fa12df1d32e7aa788a22a
stephanie rousset <strabon@club-internet.fr>
2023-11-20 17:08:51 +0100

    add switch button for language

---
+++ app/javascript/controllers/locales_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="locales"
+export default class extends Controller {
+  static targets = ["select"]
+  connect() {
+    //console.log(this.element)
+    //console.log(this.selectTarget)
+  }
+  changeLanguage(event){
+    event.preventDefault()
+    const language = this.selectTarget.value
+    const url = new URL(window.location)
+    const parts = url.pathname.split('/')
+    // if locale is fr or if there is no locale yet => change for en
+    if (parts.includes('fr') || !parts.includes('en')){
+      parts.splice(1, 1, language)
+      url.pathname = parts.join('/')
+      window.location = url.href  //reload the page with new url
+    // if locale is en
+    }else if (parts.includes('en')){
+      parts.splice(1, 1, language)
+      url.pathname = parts.join('/')
+      window.location = url.href
+    }
+  }
+}
