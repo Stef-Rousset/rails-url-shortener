ad030b6dc4f12e1f46f46985468e442641c0a39b
stephanie rousset <strabon@club-internet.fr>
2023-12-04 10:24:47 +0100

    add tests for transaction model methods

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,12 +1,25 @@
 FactoryBot.define do
   factory :transaction do
-    payee { "MyString" }
-    amount { 9.99 }
-    date { "2023-11-29" }
-    type { 1 }
-    description { "MyText" }
-    checked { false }
-    account { nil }
-    categorie { nil }
+    factory :transaction1 do
+      payee { "électricité" }
+      amount { 49.99 }
+      date { "2023-11-29" }
+      transaction_type { 0 }
+      description { "novembre 2023" }
+      checked { false }
+      account { nil }
+      category { nil }
+    end
+
+    factory :transaction2 do
+      payee { "cinéma" }
+      amount { 9.99 }
+      date { "2023-12-01" }
+      transaction_type { 0 }
+      description { "" }
+      checked { false }
+      account { nil }
+      category { nil }
+    end
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe Transaction, type: :model do
     trans = Transaction.new(payee: 'Paul', date: Date.today, amount: 10, transaction_type: 0,account_id: @account.id)
     expect(trans).to be_valid
   end
+
+  it 'gives the previous transaction if there is one or nil if there is not' do
+    transaction = create(:transaction1, account: @account) # date 29 nov
+    second = create(:transaction2, account: @account) # date 1 dec
+    expect(transaction.previous_transaction).to eq(second)
+    expect(second.previous_transaction).to eq(nil)
+  end
+
+  it 'updates balance account when transaction is saved' do
+    expect(@account.balance).to eq(9.99)
+    transaction = Transaction.new(payee: 'Paul', date: Date.today, amount: 10, transaction_type: 1, account_id: @account.id)
+    expect(@account.balance).to eq(9.99)
+    transaction.save
+    @account.reload
+    expect(@account.balance).to eq(19.99)
+  end
 end
