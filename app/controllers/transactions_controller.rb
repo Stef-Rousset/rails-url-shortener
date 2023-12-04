90636f968aa2326e2444d3cb1c2234ff6385301f
stephanie rousset <strabon@club-internet.fr>
2023-12-04 17:29:36 +0100

    add details sitmulus controller to see transaction details and add destroy transaction button

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,26 +1,41 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_account, only: %i[new create]
 
   def new
-    @account = Account.find(params[:account_id])
     @transaction = Transaction.new
   end
 
   def create
-    @account = Account.find(params[:account_id])
     @transaction = Transaction.new(transaction_params)
     @transaction.account = @account
     if @transaction.save
       respond_to do |format|
         format.html { redirect_to account_path(@account), notice: t(:created, name: t(:transaction)) }
-        format.turbo_stream { flash.now[:notice] = 'Opération créee !' }
+        format.turbo_stream { flash.now[:notice] = t(:created, name: t(:transaction)) }
       end
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+    @account = @transaction.account
+    @transaction.update_balance_after_destroy
+    #logger.debug "balance: #{@account.balance}"
+    respond_to do |format|
+      format.html { redirect_to account_path(@account), notice: t(:destroyed, name: t(:transaction)) }
+      format.turbo_stream { flash.now[:notice] = t(:destroyed, name: t(:transaction)) }
+    end
+  end
+
   private
+
+  def set_account
+    @account = Account.find(params[:account_id])
+  end
 
   def transaction_params
     params.require(:transaction).permit(:payee, :amount, :transaction_type, :description, :date, :checked, :category_id)
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,11 +5,23 @@ class Transaction < ApplicationRecord
   after_save :update_balance_account
 
   enum transaction_type: { debit: 0, credit: 1 }
+  scope :ordered, -> { order(date: :desc, created_at: :desc) }
 
   validates :transaction_type, :date, :payee, presence: true
 
   def previous_transaction
-    account.transactions.order(date: :desc, created_at: :desc).where('date > ?', self.date).last
+    account.transactions.ordered.where('date > ?', self.date).last
+  end
+
+  def update_balance_after_destroy
+    account = self.account
+    balance = account.balance
+    if self.transaction_type == 'credit'
+      balance -= self.amount
+    else
+      balance += self.amount
+    end
+    account.update(balance: balance)
   end
 
   private
