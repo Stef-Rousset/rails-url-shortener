13697cd68a3781fd564bd7209ecec684673bd5bc
stephanie rousset <strabon@club-internet.fr>
2024-01-03 15:55:24 +0100

    change card animation in home page view and update stimulus controllers and translations

--- app/javascript/controllers/back_to_top_controller.js
+++ app/javascript/controllers/back_to_top_controller.js
@@ -11,13 +11,26 @@ export default class extends Controller {
 
   showButton(){
     const body = this.bodyTarget
+    const scrolledPixs = body.scrollTop
     const button = this.buttonTarget
-    if (body.scrollTop >= 150){  //scrollTop renvoit le nb de px dont l'élement est scrollé
+    if (scrolledPixs >= 150){  //scrollTop renvoit le nb de px dont l'élement est scrollé
       button.classList.remove('hidden')
-    } else if (body.scrollTop <= 150){
+    } else if (scrolledPixs <= 150){
       button.classList.add('hidden')
     }
+    // depending on scroll, make home page divs appear
+    if (scrolledPixs > 900){
+      document.getElementById("fifthdiv").classList.add("animate-animatediv")
+      document.getElementById("fifthp").classList.add("animate-animatep")
+    }else if (scrolledPixs > 600){
+      document.getElementById("fourthdiv").classList.add("animate-animatediv")
+      document.getElementById("fourthp").classList.add("animate-animatep")
+    }else if (scrolledPixs > 300) {
+      document.getElementById("thirddiv").classList.add("animate-animatediv")
+      document.getElementById("thirdp").classList.add("animate-animatep")
+    }
   }
+
   upToTop(){
     const body = this.bodyTarget
     body.scrollTo({
--- app/javascript/controllers/cards_controller.js
+++ app/javascript/controllers/cards_controller.js
@@ -2,19 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="cards"
 export default class extends Controller {
+   static targets = ["firstdiv", "firstp", "seconddiv", "secondp"]
 
   connect() {
     //console.log(this.element)
-  }
-  toggleText(event){
-    const img = event.currentTarget.children[0]
-    const textOn = event.currentTarget.children[1]
-    const textOff = event.currentTarget.children[2]
-    img.classList.toggle("hidden")
-    textOn.classList.toggle("hidden")
-    textOff.classList.toggle("hidden")
-    //img.classList.toggle("invert")
-    event.currentTarget.classList.toggle("bg-beige")
-    event.currentTarget.classList.toggle("bg-darkblue")
+    const firstdiv = this.firstdivTarget
+    const firstp = this.firstpTarget
+    const seconddiv = this.seconddivTarget
+    const secondp = this.secondpTarget
+    firstdiv.classList.add("animate-animatediv")
+    firstp.classList.add("animate-animatep")
+    setTimeout(() => {
+      seconddiv.classList.add("animate-animatediv")
+      secondp.classList.add("animate-animatep")
+    }, 500);
   }
 }
