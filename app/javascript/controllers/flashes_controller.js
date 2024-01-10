2d0cef5df6a75058d6bc718c5b80315f454af589
stephanie rousset <strabon@club-internet.fr>
2024-01-10 15:20:18 +0100

    modify flashes controller js

--- app/javascript/controllers/flashes_controller.js
+++ app/javascript/controllers/flashes_controller.js
@@ -5,13 +5,18 @@ export default class extends Controller {
   static targets = ["flash"]
 
   connect() {
-    // console.log(this.element.firstElementChild)
-    // error in console when using this.flashTarget if flash isn't present
-    if (this.element.firstElementChild !== null && this.flashTarget.role === 'notice') {
-        setTimeout(() => {
-            this.flashTarget.classList.add('animate-flashdisappear')
-        }, 3000);
-    }
+      // console.log(this.element.firstElementChild)
+      // error in console when using this.flashTarget if flash isn't present
+      if (this.element.firstElementChild !== null && this.flashTarget.role === 'notice') {
+          // div fades
+          setTimeout(() => {
+                this.flashTarget.classList.add('animate-flashdisappear')
+            }, 3000);
+          // div is suppressed 50ms after end of animate-flashdisappear
+          setTimeout(() => {
+            this.flashTarget.style.display = "none"
+          }, 3550);
+      }
   }
   close(){
       this.flashTarget.style.display = "none"
